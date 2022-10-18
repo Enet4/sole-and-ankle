17a8c581b2d0518814719b65484bb805387a7dfe
Eduardo Pinho <enet4mikeenet@gmail.com>
2022-10-18 17:19:07 +0100

    Module 4 resolution
    Known caveats:
    
    - Not sure if the superheader is done right,
      because it is unclear
      whether that should stick to the top of the page all the time,
      and I couldn't do that
    - Paddings and flex bases in ShoeCard were a bit done by eye
      based on what looked good enough,
      and some edge cases might feel a bit roughly done
    - Font size of sale info was set to 10pt,
      which might not be 100% as in the Figma design
    
    Misc:
    
    - used dynamic styles for the price tags and article variants
    - had to create another styled component called VariantTip
      for the Sale/New info

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,19 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 21px 32px 19px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  margin-left: auto;
+  flex-grow: 1;
+  display: flex;
+  gap: 48px;
+  justify-content: center;
+  align-items: center;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -25,7 +25,8 @@ const ShoeCard = ({
   // both on-sale and new-release, but in this case, `on-sale`
   // will triumph and be the variant used.
   // prettier-ignore
-  const variant = typeof salePrice === 'number'
+  const sale = typeof salePrice === 'number';
+  const variant = sale
     ? 'on-sale'
     : isNewShoe(releaseDate)
       ? 'new-release'
@@ -40,11 +41,16 @@ const ShoeCard = ({
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price sale={sale}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {sale && (<SalePrice>{formatPrice(salePrice)}</SalePrice>)}
         </Row>
+        {variant !== 'default' && (<VariantTip variant={variant}>{{
+          'on-sale': 'Sale',
+          'new-release': 'Just released!'
+        }[variant]}</VariantTip>)}
       </Wrapper>
     </Link>
   );
@@ -53,18 +59,33 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1 1 280px;
+  max-width: 600px;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
+  width: 100%;
+  background-color: #f2f3f3;
+  border-radius: 16px 16px 4px 4px;
+  padding-top: 40px;
+  padding-bottom: 32px;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -72,7 +93,10 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  color: ${props => props.sale ? '#60666c': 'inherit'};
+  text-decoration: ${props => props.sale ? 'line-through': 'none'};
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
@@ -81,6 +105,24 @@ const ColorInfo = styled.p`
 const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
+`;
+
+const VariantTip = styled.span`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  background-color: ${({variant}) => ({
+    'on-sale': '#C5295D',
+    'new-release': '#6868D9'
+    }[variant])};
+  text-align: right;
+  border-radius: 2px;
+  font-family: 'Raleway';
+  font-size: 10pt;
+  font-weight: 700;
+  line-height: 16px;
+  padding: 10px 7px;
+  color: #fff;
 `;
 
 export default ShoeCard;
